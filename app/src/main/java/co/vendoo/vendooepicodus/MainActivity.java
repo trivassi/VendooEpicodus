f9c9e3682ec98129f03200d5cd0f35d3735f6a9f
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-18 11:57:11 -0800

    create CreateAccountActivity

--- app/src/main/java/co/vendoo/vendooepicodus/MainActivity.java
+++ app/src/main/java/co/vendoo/vendooepicodus/MainActivity.java
@@ -9,19 +9,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mlogInButton;
-    private Button mcreateAccountButton;
-
-
+    @Bind(R.id.logInButton) Button mLogInButton;
+    @Bind(R.id.createAccountButton) Button mCreateAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLogInButton.setOnClickListener(this);
+        mCreateAccountButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == mLogInButton) {
+            Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "Log in with Facebook coming soon!", Toast.LENGTH_LONG).show();
+        }
+
+        if (v == mCreateAccountButton) {
+            Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "Log in with Facebook coming soon!", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+
 }
