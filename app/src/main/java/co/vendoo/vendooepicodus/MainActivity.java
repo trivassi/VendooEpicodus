4a0af63570d8f6657b6e44a7e3f1a828cca2252b
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-18 11:16:36 -0800

    add login button

--- app/src/main/java/co/vendoo/vendooepicodus/MainActivity.java
+++ app/src/main/java/co/vendoo/vendooepicodus/MainActivity.java
@@ -1,7 +1,16 @@
 package co.vendoo.vendooepicodus;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
