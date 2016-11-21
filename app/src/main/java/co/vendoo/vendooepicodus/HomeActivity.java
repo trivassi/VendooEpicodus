9dddf95395be080eeff2c1aae8d431edc0937708
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-20 23:45:02 -0500

    add function to buy and sell functions

--- app/src/main/java/co/vendoo/vendooepicodus/HomeActivity.java
+++ app/src/main/java/co/vendoo/vendooepicodus/HomeActivity.java
@@ -1,13 +1,45 @@
 package co.vendoo.vendooepicodus;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Bind(R.id.buyButton) Button mBuyButton;
+    @Bind(R.id.sellButton) Button mSellButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mBuyButton.setOnClickListener(this);
+        mSellButton.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == mBuyButton || v == mSellButton) {
+            Intent intent = new Intent(HomeActivity.this, MarketplacesActivity.class);
+            //            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+
+    }
+
 }
