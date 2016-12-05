f8d86c97cd73f1bef444340bc6d2cae0bf253f40
Thomas Rivas <rivassi.thomas@gmail.com>
2016-12-04 23:40:00 -0800

    get response from yelp onto log

---
+++ app/src/main/java/co/vendoo/vendooepicodus/Store.java
@@ -0,0 +1,64 @@
+package co.vendoo.vendooepicodus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by T on 12/4/16.
+ */
+
+public class Store {
+    private String mName;
+    private String mPhone;
+    private String mWebsite;
+    private double mRating;
+    private String mImageUrl;
+    private ArrayList<String> mAddress = new ArrayList<>();
+    private double mLatitude;
+    private double mLongitude;
+
+    public Store(String name, String phone, String website,
+                      double rating, String imageUrl, ArrayList<String> address,
+                      double latitude, double longitude) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+}
--- app/src/main/java/co/vendoo/vendooepicodus/ui/StoresActivity.java
+++ app/src/main/java/co/vendoo/vendooepicodus/ui/StoresActivity.java
@@ -3,12 +3,21 @@ package co.vendoo.vendooepicodus.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.vendoo.vendooepicodus.R;
+import co.vendoo.vendooepicodus.Store;
+import co.vendoo.vendooepicodus.YelpService;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class StoresActivity extends AppCompatActivity {
 
@@ -16,6 +25,8 @@ public class StoresActivity extends AppCompatActivity {
     public static final String TAG = StoresActivity.class.getSimpleName();
 
     @Bind(R.id.listView) ListView mListView;
+    public ArrayList<Store> mStores = new ArrayList<>();
+
 
 
     @Override
@@ -30,5 +41,49 @@ public class StoresActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, marketplaces);
         mListView.setAdapter(adapter);
 
+        getStores(location);
+    }
+
+    private void getStores(String location) {
+        final YelpService yelpService = new YelpService();
+
+        yelpService.findStores(location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mStores = yelpService.processResults(response);
+
+                StoresActivity.this.runOnUiThread(new Runnable() {
+
+
+                    @Override
+                    public void run() {
+                        String[] storeNames = new String[mStores.size()];
+                        for (int i = 0; i < storeNames.length; i++) {
+                            storeNames[i] = mStores.get(i).getName();
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(StoresActivity.this, android.R.layout.simple_list_item_1, storeNames);
+                        mListView.setAdapter(adapter);
+
+                        for (Store store : mStores) {
+                            Log.d(TAG, "Name: " + store.getName());
+                            Log.d(TAG, "Phone: " + store.getPhone());
+                            Log.d(TAG, "Website: " + store.getWebsite());
+                            Log.d(TAG, "Image url: " + store.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(store.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", store.getAddress()));
+                        }
+
+                    }
+                }); //runonuithreas
+
+            }//on response
+
+        }); //callback
     }
 }
