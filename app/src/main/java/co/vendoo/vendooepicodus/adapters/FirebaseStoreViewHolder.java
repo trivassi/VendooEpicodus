4ea278558b017fde7e43d710ab9b71b3f470306e
Thomas Rivas <rivassi.thomas@gmail.com>
2016-12-20 23:13:29 -0800

    succesfully drag, drop and keep order of saved stores

--- app/src/main/java/co/vendoo/vendooepicodus/adapters/FirebaseStoreViewHolder.java
+++ app/src/main/java/co/vendoo/vendooepicodus/adapters/FirebaseStoreViewHolder.java
@@ -1,8 +1,11 @@
 package co.vendoo.vendooepicodus.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,12 +25,13 @@ import co.vendoo.vendooepicodus.Constants;
 import co.vendoo.vendooepicodus.R;
 import co.vendoo.vendooepicodus.models.Store;
 import co.vendoo.vendooepicodus.ui.StoreDetailActivity;
+import co.vendoo.vendooepicodus.util.ItemTouchHelperViewHolder;
 
 /**
  * Created by T on 12/12/16.
  */
 
-public class FirebaseStoreViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseStoreViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
 
@@ -58,6 +62,57 @@ public class FirebaseStoreViewHolder extends RecyclerView.ViewHolder {
         ratingTextView.setText("Rating: " + store.getRating() + "/5");
     }
 
+    @Override
+    public void onItemSelected() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
+
+        //programmatic
+//        itemView.animate()
+//                .alpha(0.7f)
+//                .scaleX(0.9f)
+//                .scaleY(0.9f)
+//                .setDuration(500);
+    }
+
+    @Override
+    public void onItemClear() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
+        //programmatic
+//        itemView.animate()
+//                .alpha(1f)
+//                .scaleX(1f)
+//                .scaleY(1f);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    @Override
 //    public void onClick(View view) {
 //        final ArrayList<Store> stores = new ArrayList<>();
@@ -85,4 +140,4 @@ public class FirebaseStoreViewHolder extends RecyclerView.ViewHolder {
 //        });
 //    }
 
-}
+
---
+++ app/src/main/java/co/vendoo/vendooepicodus/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package co.vendoo.vendooepicodus.util;
+
+/**
+ * Created by T on 12/20/16.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
